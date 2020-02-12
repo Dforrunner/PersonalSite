adf790090233dfbc280194d98dfb8d627208dcfa
mbarut1 <vtme996@gmail.com>
2020-02-12 08:37:27 -0500

    Changed the Webpack settings. And install some dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,36 @@
+var path = require("path");
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    plugins: [
-        new Dotenv()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    }
+  context: __dirname,
+
+  entry: './mySite/src/index.js',
+
+  output: {
+      path: path.join(__dirname, '/mySite/static/mySite/scripts/'),
+      filename: "main.js",
+  },
+
+  plugins: [
+      new Dotenv(),
+      new UglifyJsPlugin(),
+      new BundleAnalyzerPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+
 };
